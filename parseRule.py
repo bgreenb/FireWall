76f4835c6ca23c7e70653dbdfa626ed79c0b900d
Kaiyue <kay029@eng.ucsd.edu>
2016-04-20 15:26:19 -0700

    test insert rule

--- parseRule.py
+++ parseRule.py
@@ -3,32 +3,33 @@ import iptc
 
 def initDic():
     d = {}
-    d['-m'] = None
-    d['-p'] = None
-    d['-src-range'] = None
-    d['-dst-range'] = None
-    d['-sport'] = None
-    d['-dport'] = None
-    d['-j'] = None
+    d['-m'] = ''
+    d['-p'] = ''
+    d['-src-range'] = ''
+    d['-dst-range'] = ''
+    d['-sport'] = ''
+    d['-dport'] = ''
+    d['-j'] = ''
+    return d
 
 def createRule(cmdDic):
     rule = iptc.Rule()
-    if !cmdDic['-p']:
+    if cmdDic['-p'] != '':
         rule.protocol = cmdDic['-p']
         match = iptc.Match(rule, cmdDic['-p'])
-        if !cmdDic['-dport']:
+        if cmdDic['-dport'] != '':
             match.dport = cmdDic['-dport']
-        if !cmdDic['-sport']:
+        if cmdDic['-sport'] != '':
             match.sport = cmdDic['-sport']
         rule.add_match(match)
-    if !cmdDic['-m']:
+    if cmdDic['-m'] != '':
         match = iptc.Match(rule, cmdDic['-m'])
-        if !cmdDic['-src-range']:
+        if cmdDic['-src-range'] != '':
             match.src_range = cmdDic['-src-range']
-        if !cmdDic['-dst-range']:
+        if cmdDic['-dst-range'] != '':
             match.dst_range = cmdDic['-dst-range']
         rule.add_match(match)
-    if !cmdDic['-j']:
+    if cmdDic['-j'] != '':
         rule.target = iptc.Target(rule, cmdDic['-j'])
 
     return rule
@@ -38,13 +39,16 @@ with open('rules.txt', 'r') as f:
     for line in f.readlines():
         #print(line.strip())
         args = line.strip().split()
-        cmdDic = initDic()
+       	print 'args: ', args
+
+ 	cmdDic = initDic()
         if args[0] != 'iptables':
             continue
         chain_name = args[2]
         for i in range(3, len(args)):
-            if args[i].startwith('-'):
-                cmdDic[args[i]] = args[i+1]
+            if args[i].startswith('-'):
+		print args[i] 
+		cmdDic[args[i]] = args[i+1]
 
         rule = createRule(cmdDic)
         chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), chain_name)
